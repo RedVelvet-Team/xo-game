b2bc139e37afd4df4b7d1525c5c0cf5b929e6009
Bassam <bassamouf@std.mans.edu.eg>
2023-07-16 01:00:51 +0300

    add packages and develop branch

---
+++ app/src/main/java/com/redvelvet/xogame/app/Application.kt
@@ -0,0 +1,4 @@
+package com.redvelvet.xogame.app
+
+class Application {
+}
--- app/src/main/java/com/redvelvet/xogame/app/MainActivity.kt
+++ app/src/main/java/com/redvelvet/xogame/app/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.redvelvet.xogame
+package com.redvelvet.xogame.app
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -10,7 +10,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.redvelvet.xogame.ui.theme.XogameTheme
+import com.redvelvet.xogame.app.ui.theme.XogameTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
