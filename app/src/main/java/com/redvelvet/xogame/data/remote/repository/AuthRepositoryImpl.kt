b6d8993ef8bd367e82b75ef62dd0bd4c93961fea
Abanoub <abanoubnagy24@gmail.com>
2023-07-16 21:31:02 +0300

    implement code for all functions of sign-in auth

--- app/src/main/java/com/redvelvet/xogame/data/remote/repository/AuthRepositoryImpl.kt
+++ app/src/main/java/com/redvelvet/xogame/data/remote/repository/AuthRepositoryImpl.kt
@@ -1,18 +1,30 @@
 package com.redvelvet.xogame.data.remote.repository
 
+import android.content.Context
+import android.content.Intent
+import android.content.IntentSender
+import com.google.android.gms.auth.api.identity.BeginSignInRequest
+import com.google.android.gms.auth.api.identity.SignInClient
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
+import com.redvelvet.xogame.R
 import com.redvelvet.xogame.app.util.Response
+import com.redvelvet.xogame.domain.entity.SignInResult
+import com.redvelvet.xogame.domain.entity.UserData
 import com.redvelvet.xogame.domain.repository.AuthRepository
 import dagger.Module
+import kotlinx.coroutines.CancellationException
 import kotlinx.coroutines.tasks.await
 import javax.inject.Inject
 
 @Module
-class AuthRepositoryImpl  @Inject constructor(
+class AuthRepositoryImpl @Inject constructor(
     private val auth: FirebaseAuth,
     private val email: String,
     private val password: String,
-): AuthRepository {
+    private val context: Context,
+    private val oneTapClient: SignInClient
+) : AuthRepository {
 
     override suspend fun firebaseSignUpAnonymously() = try {
         auth.createUserWithEmailAndPassword(email, password).await()
@@ -21,7 +33,7 @@ class AuthRepositoryImpl  @Inject constructor(
         Response.Failure(e)
     }
 
-    override suspend fun firebaseLogIn()= try {
+    override suspend fun firebaseLogIn() = try {
         auth.signInWithEmailAndPassword(email, password).await()
         Response.Success(true)
     } catch (e: Exception) {
@@ -33,5 +45,76 @@ class AuthRepositoryImpl  @Inject constructor(
         Response.Success(true)
     } catch (e: Exception) {
         Response.Failure(e)
+    }
+
+
+    override suspend fun signIn(): IntentSender? {
+        val result = try {
+            oneTapClient.beginSignIn(
+                buildSignInRequest()
+            ).await()
+        } catch (e: Exception) {
+            e.printStackTrace()
+            if (e is CancellationException) throw e
+            null
+        }
+        return result?.pendingIntent?.intentSender
+    }
+
+    override suspend fun signInWithIntent(intent: Intent): SignInResult {
+        val credential = oneTapClient.getSignInCredentialFromIntent(intent)
+        val googleIdToken = credential.googleIdToken
+        val googleCredentials = GoogleAuthProvider.getCredential(googleIdToken, null)
+        return try {
+            val user = auth.signInWithCredential(googleCredentials).await().user
+            SignInResult(
+                data = user?.run {
+                    UserData(
+                        userId = uid,
+                        username = displayName,
+                        profilePictureUrl = photoUrl?.toString()
+                    )
+                },
+                errorMessage = null
+            )
+        } catch (e: Exception) {
+            e.printStackTrace()
+            if (e is CancellationException) throw e
+            SignInResult(
+                data = null,
+                errorMessage = e.message
+            )
+        }
+    }
+
+    override suspend fun signOut() {
+        try {
+            oneTapClient.signOut().await()
+            auth.signOut()
+        } catch (e: Exception) {
+            e.printStackTrace()
+            if (e is CancellationException) throw e
+        }
+    }
+
+    override suspend fun getSignedInUser(): UserData? = auth.currentUser?.run {
+        UserData(
+            userId = uid,
+            username = displayName,
+            profilePictureUrl = photoUrl?.toString()
+        )
+    }
+
+    override suspend fun buildSignInRequest(): BeginSignInRequest {
+        return BeginSignInRequest.Builder()
+            .setGoogleIdTokenRequestOptions(
+                BeginSignInRequest.GoogleIdTokenRequestOptions.builder()
+                    .setSupported(true)
+                    .setFilterByAuthorizedAccounts(false)
+                    .setServerClientId(context.getString(R.string.web_client_id))
+                    .build()
+            )
+            .setAutoSelectEnabled(true)
+            .build()
     }
 }
