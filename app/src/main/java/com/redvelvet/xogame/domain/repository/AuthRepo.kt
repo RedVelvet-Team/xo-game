821e0d934a24152428ce84241397513b1ed58175
Abanoub <abanoubnagy24@gmail.com>
2023-07-16 22:13:26 +0300

    remove google auth from auth repository

--- app/src/main/java/com/redvelvet/xogame/domain/repository/AuthRepo.kt
+++ app/src/main/java/com/redvelvet/xogame/domain/repository/AuthRepo.kt
@@ -1,12 +1,7 @@
 package com.redvelvet.xogame.domain.repository
 
 
-import android.content.Intent
-import android.content.IntentSender
-import com.google.android.gms.auth.api.identity.BeginSignInRequest
 import com.redvelvet.xogame.app.util.Response
-import com.redvelvet.xogame.domain.entity.SignInResult
-import com.redvelvet.xogame.domain.entity.UserData
 
 
 typealias SignUpResponse = Response<Boolean>
@@ -18,9 +13,4 @@ interface AuthRepository {
     suspend fun firebaseLogIn(): LogInResponse
     suspend fun firebaseSignOut(): SignOutResponse
 
-    suspend fun signIn(): IntentSender?
-    suspend fun signInWithIntent(intent: Intent): SignInResult
-    suspend fun signOut()
-    suspend fun getSignedInUser(): UserData?
-    suspend fun buildSignInRequest(): BeginSignInRequest
 }
