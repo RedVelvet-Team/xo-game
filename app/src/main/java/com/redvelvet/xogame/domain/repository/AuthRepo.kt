4ee165e2f0da8d72f9132d71151d50b4ce5de76f
Abanoub <abanoubnagy24@gmail.com>
2023-07-16 21:30:11 +0300

    implement all suspend function for auth gmail sign-in

--- app/src/main/java/com/redvelvet/xogame/domain/repository/AuthRepo.kt
+++ app/src/main/java/com/redvelvet/xogame/domain/repository/AuthRepo.kt
@@ -1,8 +1,12 @@
 package com.redvelvet.xogame.domain.repository
 
 
+import android.content.Intent
+import android.content.IntentSender
+import com.google.android.gms.auth.api.identity.BeginSignInRequest
 import com.redvelvet.xogame.app.util.Response
-
+import com.redvelvet.xogame.domain.entity.SignInResult
+import com.redvelvet.xogame.domain.entity.UserData
 
 
 typealias SignUpResponse = Response<Boolean>
@@ -11,6 +15,12 @@ typealias SignOutResponse = Response<Boolean>
 
 interface AuthRepository {
     suspend fun firebaseSignUpAnonymously(): SignUpResponse
-    suspend fun firebaseLogIn():LogInResponse
+    suspend fun firebaseLogIn(): LogInResponse
     suspend fun firebaseSignOut(): SignOutResponse
+
+    suspend fun signIn(): IntentSender?
+    suspend fun signInWithIntent(intent: Intent): SignInResult
+    suspend fun signOut()
+    suspend fun getSignedInUser(): UserData?
+    suspend fun buildSignInRequest(): BeginSignInRequest
 }
