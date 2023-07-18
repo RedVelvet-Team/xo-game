9f500405fd0be4b5e596b6b9a5324771bdf9d917
Bassam <bassamouf@std.mans.edu.eg>
2023-07-18 22:53:33 +0300

    add hilt view model annotation

--- app/src/main/java/com/redvelvet/xogame/presentation/screens/signin/SignInViewModel.kt
+++ app/src/main/java/com/redvelvet/xogame/presentation/screens/signin/SignInViewModel.kt
@@ -1,10 +1,12 @@
 package com.redvelvet.xogame.presentation.screens.signin
 
 import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
 
+@HiltViewModel
 class SignInViewModel:ViewModel() {
 
     private val _state = MutableStateFlow(SignInState())
