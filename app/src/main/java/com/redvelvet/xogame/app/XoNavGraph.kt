e0a9daaa28edafdf154be9ccbb2b1fb81f342ca1
Haidy <haidyabugom3a@gmail.com>
2023-07-20 01:50:02 +0300

    add navigation in login activity

--- app/src/main/java/com/redvelvet/xogame/app/XoNavGraph.kt
+++ app/src/main/java/com/redvelvet/xogame/app/XoNavGraph.kt
@@ -3,9 +3,11 @@ package com.redvelvet.xogame.app
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
+import com.redvelvet.xogame.presentation.screens.home.homeRoute
 
 @Composable
 fun XoNavGraph(navController: NavHostController) {
-    NavHost(navController = navController, startDestination = "Splash") {
+    NavHost(navController = navController, startDestination = "home") {
+        homeRoute(navController)
     }
 }
