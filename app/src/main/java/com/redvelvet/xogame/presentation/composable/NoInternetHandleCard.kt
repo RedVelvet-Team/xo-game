d4f0abf38e6d7141081a2d7459a26ead817f9970
kamel <kikokeko10@gmail.com>
2023-07-19 19:31:44 +0300

    finish auth

--- app/src/main/java/com/redvelvet/xogame/presentation/composable/NoInternetHandleCard.kt
+++ app/src/main/java/com/redvelvet/xogame/presentation/composable/NoInternetHandleCard.kt
@@ -21,11 +21,13 @@ import androidx.compose.ui.draw.paint
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.redvelvet.xogame.R
 
 @Composable
+@Preview(showSystemUi = true)
 fun NoInternetHandle() {
     Column(
         horizontalAlignment = Alignment.CenterHorizontally,
