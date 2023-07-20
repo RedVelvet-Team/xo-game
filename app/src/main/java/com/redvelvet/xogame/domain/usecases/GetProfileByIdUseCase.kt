8f73c96f6ccdc731a8ea3e002e15f75575734e65
Haidy <haidyabugom3a@gmail.com>
2023-07-20 13:01:02 +0300

    handle nullable result in usecases

--- app/src/main/java/com/redvelvet/xogame/domain/usecases/GetProfileByIdUseCase.kt
+++ app/src/main/java/com/redvelvet/xogame/domain/usecases/GetProfileByIdUseCase.kt
@@ -1,11 +1,26 @@
 package com.redvelvet.xogame.domain.usecases
 
+import com.redvelvet.xogame.domain.entity.UserEntity
 import com.redvelvet.xogame.domain.repository.AuthGoogleRepository
 import javax.inject.Inject
 
 class GetProfileByIdUseCase @Inject constructor(
     private val authGoogleRepository: AuthGoogleRepository
 ) {
-    suspend fun invoke(userId: String) =
-        authGoogleRepository.getUserById(userId)
+    suspend operator fun invoke(userId: String) =
+        authGoogleRepository.getUserById(userId) ?: UserEntity(
+            id = "",
+            name = "",
+            profilePictureUrl = "",
+            friends = emptyList(),
+            draw = 0,
+            email = "",
+            friendsCount = 0,
+            lost = 0,
+            gamePlayed = 0,
+            status = "",
+            won = 0,
+            friendRequest = emptyList(),
+            friendRequestCount = 0
+        )
 }
