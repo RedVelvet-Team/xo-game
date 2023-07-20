f0a23fabc45918327847edecc4077f4e1481ca08
Haidy <haidyabugom3a@gmail.com>
2023-07-20 13:23:46 +0300

    fix background

--- app/src/main/java/com/redvelvet/xogame/presentation/screens/profile/personal/ProfileScreen.kt
+++ app/src/main/java/com/redvelvet/xogame/presentation/screens/profile/personal/ProfileScreen.kt
@@ -13,7 +13,9 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -45,8 +47,10 @@ fun ProfileContent(
         modifier = Modifier.fillMaxSize()
     ) {
         Image(
-            painter = painterResource(id = R.drawable.background_img),
-            contentDescription = "background"
+            painter = painterResource(id = R.drawable.image_background),
+            contentDescription = stringResource(R.string.background),
+            modifier = Modifier.fillMaxSize(),
+            contentScale = ContentScale.FillWidth
         )
         Column(
             modifier = Modifier
