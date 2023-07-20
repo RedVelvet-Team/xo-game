5cd01931e5e965879135aa88ff79bd1cec414bb2
Haidy <haidyabugom3a@gmail.com>
2023-07-20 13:05:32 +0300

    finish profile screen

--- app/src/main/java/com/redvelvet/xogame/presentation/screens/profile/personal/ProfileScreen.kt
+++ app/src/main/java/com/redvelvet/xogame/presentation/screens/profile/personal/ProfileScreen.kt
@@ -6,30 +6,48 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.redvelvet.xogame.presentation.composable.ProfileCard
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavController
 import com.example.dountapplication.screen.VerticalSpacer
 import com.redvelvet.xogame.R
 import com.redvelvet.xogame.presentation.composable.OneFriendRequestCard
 import com.redvelvet.xogame.presentation.composable.ProfileAppbar
+import com.redvelvet.xogame.presentation.composable.ProfileCard
+import com.redvelvet.xogame.presentation.screens.profile.personal.PersonalProfileUiState
+import com.redvelvet.xogame.presentation.screens.profile.personal.PersonalProfileViewModel
+import com.redvelvet.xogame.presentation.screens.profile.users.navigateToUserProfile
 
 @Composable
-fun ProfileScreen(){
-
+fun ProfileScreen(
+    navController: NavController,
+    viewModel: PersonalProfileViewModel = hiltViewModel()
+) {
+    val state by viewModel.state.collectAsState()
+    ProfileContent(state = state, onClickAvatar = { id -> navController.navigateToUserProfile(id) })
 }
 
 @Composable
-fun ProfileContent(){
+fun ProfileContent(
+    state: PersonalProfileUiState,
+    onClickAvatar: (String) -> Unit
+) {
     Box(
         modifier = Modifier.fillMaxSize()
     ) {
-        Image(painter = painterResource(id = R.drawable.background_img), contentDescription = "background" )
+        Image(
+            painter = painterResource(id = R.drawable.background_img),
+            contentDescription = "background"
+        )
         Column(
             modifier = Modifier
                 .fillMaxSize()
@@ -38,15 +56,13 @@ fun ProfileContent(){
             VerticalSpacer(space = 48)
             ProfileAppbar()
             VerticalSpacer(space = 56)
-            ProfileCard()
+            ProfileCard(state)
             VerticalSpacer(space = 32)
             LazyColumn(
                 modifier = Modifier.clip(shape = RoundedCornerShape(16.dp)),
             ) {
-                item{
-                    OneFriendRequestCard()
-                    OneFriendRequestCard()
-                    OneFriendRequestCard()
+                items(state.friendRequests) {
+                    OneFriendRequestCard(state = it, onClickAvatar = onClickAvatar)
                 }
             }
         }
@@ -54,8 +70,9 @@ fun ProfileContent(){
     }
 
 }
+
 @Composable
 @Preview(widthDp = 360, heightDp = 800)
-fun PreviewProfileContent(){
-    ProfileContent()
+fun PreviewProfileContent() {
+    //ProfileContent()
 }
